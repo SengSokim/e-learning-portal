b4f9dc331dde9f01ffd1be886214707ee2d2a80f
Sokim <sengsokim88@gmail.com>
2024-03-13 22:52:58 +0700

    fix

--- app/(landing_page)/main/page.js
+++ app/(landing_page)/main/page.js
@@ -51,41 +51,41 @@ function Main() {
         className={`lg:mx-[200px] text-white grid grid-cols-2 md:grid-cols-4 p-5`}
       >
         <div className="col-span-3 mr-2 mt-3">
-          <div className="text-black bg-white flex-no-wrap sticky top-0 z-10 flex items-center space-x-4 text-sm p-3 rounded-md justify-between">
-            <div className="flex gap-5">
-              {contentCategories.map((item, index) => (
-                <Link
-                  href={item.url}
-                  key={index}
-                  className={
-                    !user && item.url == "/reading-list" ? "hidden" : ""
-                  }
-                >
-                  <div
-                    className={`hover:text-violet-600 transition-all ease-in-out duration-200 ${
-                      index == 1 && " text-violet-600"
-                    }`}
+          <Suspense fallback={<div>Loading...</div>}>
+            <div className="text-black bg-white flex-no-wrap sticky top-0 z-10 flex items-center space-x-4 text-sm p-3 rounded-md justify-between">
+              <div className="flex gap-5">
+                {contentCategories.map((item, index) => (
+                  <Link
+                    href={item.url}
+                    key={index}
+                    className={
+                      !user && item.url == "/reading-list" ? "hidden" : ""
+                    }
                   >
-                    {item.tab}
-                  </div>
-                </Link>
-              ))}
-            </div>
-            <Suspense fallback={<div>Loading...</div>}>
+                    <div
+                      className={`hover:text-violet-600 transition-all ease-in-out duration-200 ${
+                        index == 1 && " text-violet-600"
+                      }`}
+                    >
+                      {item.tab}
+                    </div>
+                  </Link>
+                ))}
+              </div>
+
               <div className="flex items-center">
                 <Input
                   placeholder="Search..."
                   value={search}
                   onChange={(e) => setSearch(e.target.value)}
                 />
-                
-                  <Button onClick={handleSearch} type="button" className="ml-3 ">
-                    <Search className="h-4 w-4" />
-                  </Button>
-                
+
+                <Button onClick={handleSearch} type="button" className="ml-3 ">
+                  <Search className="h-4 w-4" />
+                </Button>
               </div>
-            </Suspense>
-          </div>
+            </div>
+          </Suspense>
           <Separator className="bg-zinc-700 mt-3" />
           <div className="">
             <BlogCard />
