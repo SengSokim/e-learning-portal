2ec11507601825109761f14ceda767bbe3f409ef
Sokim <sengsokim88@gmail.com>
2024-03-14 16:52:55 +0700

    remove missingSuspenseWithCSRBailout

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        missingSuspenseWithCSRBailout: false,
-      },
+    
     images:{
         domains: ['media.graphassets.com']
     }
